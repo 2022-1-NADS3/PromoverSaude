a9131f82677988d5c6274d43af031a8fc24b2d8b
vruama <104402990+vruama@users.noreply.github.com>
2022-05-02 22:29:38 -0300

    Criação de novas telas

---
+++ src/Frontend/AppPSOne/Cadastro - Cópia (2).xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppPSOne
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cadastro : ContentPage
+    {
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void Voltar(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void Cad(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+
+    }
+}
