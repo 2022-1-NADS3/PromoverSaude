3476fafb9d7a52c83f16fe577963e88e0e9d767f
vruama <104402990+vruama@users.noreply.github.com>
2022-05-09 21:35:11 -0300

    teste de conexão com a api

--- src/Frontend/AppPSOne/MainPage.xaml.cs
+++ src/Frontend/AppPSOne/MainPage.xaml.cs
@@ -1,15 +1,50 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace AppPSOne
 {
+    /*-----Teste-----------*/
+    public class Objeto
+    {
+        [JsonProperty("nome")]
+        public string nome { get; set; }
+            
+        [JsonProperty("sobrenome")]
+        public string sobrenome { get; set; }            
+
+        [JsonProperty("idade")]
+        public int idade { get; set; }
+
+        [JsonProperty("altura")]
+        public float altura { get; set; }
+    }
+    /*-----Teste-----------*/
+
     public partial class MainPage : ContentPage
     {
+        /*------------ Teste -------------*/
+        async void Dados(object sender, EventArgs e)
+        {
+            var httpClient = new HttpClient();
+            var resultados = await httpClient.GetStringAsync("http://132.1.2.21:3000/user");
+            var resultadoFinal = JsonConvert.DeserializeObject<Objeto>(resultados);
+
+            json.Text = resultados;
+            userName.Text = resultadoFinal.nome;
+            sureName.Text = resultadoFinal.sobrenome;
+            userAge.Text = resultadoFinal.idade.ToString();
+            userHeight.Text = resultadoFinal.altura.ToString();
+
+        }
+        /*-----Teste-----------*/
+
         public MainPage()
         {
             InitializeComponent();
@@ -28,6 +63,7 @@ namespace AppPSOne
         {
             Navigation.PushAsync(new Perfil());
         }
+
 
     }
 }
