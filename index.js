4d5f997e566a2005f0c38c9d5aaa155a590ec4e4
renatorb-fecap <104402824+renatorb-fecap@users.noreply.github.com>
2022-05-17 14:24:40 -0300

    commit server with "/user"

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.get("/", function (req, res){
     res.send("Teste de conexão com heroku!");
 });
 
+app.get("/user", function(req,res){
+    console.log("Enviei po Get");
+    res.send(JSON.stringify({
+        nome:"Teo",
+        sobrenome:"balão",
+        idade:22,
+        altura:1.75
+    }));
+});
+
 app.listen(port, () => {
     console.info(`Aplicação rodando em http://localhost:${port}`)
 });
