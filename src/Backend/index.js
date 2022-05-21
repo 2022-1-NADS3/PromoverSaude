40cbc6f1705213e97053df9685d41805825fff89
vruama <vanessa.ruama@totvs.com.br>
2022-05-21 17:41:06 -0300

    tentativa conexão banco

--- src/Backend/index.js
+++ src/Backend/index.js
@@ -41,3 +41,67 @@ app.post("/user", function(req,res){
     
     return res.json(usuario);
 });
+
+async function connect() {
+    if (global.connection)
+        return global.connection.connect();
+ 
+    const { Pool } = require('pg');
+    const pool = new Pool({
+        connectionString: 'ec2-3-211-6-217.compute-1.amazonaws.com:5432/d1dsvje0jd710s'
+    });
+ 
+    //apenas testando a conexão
+    const client = await pool.connect();
+    console.log("Criou pool de conexões no PostgreSQL!");
+ 
+    const res = await client.query('SELECT NOW()');
+    console.log(res.rows[0]);
+    client.release();
+ 
+    //guardando para usar sempre o mesmo
+    global.connection = pool;
+    return pool.connect();
+}
+
+//index.js
+const db = require("./db");
+
+async function selectUsers() {
+    const client = await connect();
+    const res = await client.query('SELECT * FROM usuario');
+    return res.rows;
+}
+ 
+module.exports = { selectUsers }
+
+//index.js
+(async () => {
+    const db = require("./db");
+    console.log('Começou!');
+
+    console.log('SELECT * FROM usuario');
+    const user = await db.selectUsers();
+    console.log(user)
+});
+
+async function insertcaduser(caduser){
+    const client = await connect();
+    const sql = 'INSERT INTO clientes(nome,email,senha, sexo ) VALUES ($1,$2,$3, $4);';
+    const values = [caduser.nome, caduser.email, caduser.senha, caduser.sexo];
+    return await client.query(sql, values);
+}
+
+//index.js
+(async () => {
+    const db = require("./db");
+    console.log('Começou!');
+    
+    console.log('INSERT INTO usuario');
+    const result = await db.insertcaduser({nome: "Zé", email: "ze@teste.com", senha: "1234", sexo:"M"});
+    console.log(result.rowCount);
+ 
+    console.log('SELECT * FROM CLIENTES');
+    const clientes = await db.selectUsers();
+    console.log(clientes);
+})();
