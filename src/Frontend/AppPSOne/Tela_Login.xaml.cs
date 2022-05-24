87cccf8598d776fd1e7e6e08e1255ad85dec1a02
vruama <104402990+vruama@users.noreply.github.com>
2022-05-23 22:23:01 -0300

    post

--- src/Frontend/AppPSOne/Tela_Login.xaml.cs
+++ src/Frontend/AppPSOne/Tela_Login.xaml.cs
@@ -6,9 +6,25 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace AppPSOne
 {
+    public class DadosUsuario
+    {
+        [JsonProperty("useremail")]
+        public string useremail { get; set; }
+
+        [JsonProperty("username")]
+        public string username { get; set; }
+
+        [JsonProperty("userpassword")]
+        public string userpassword { get; set; }
+        [JsonProperty("usersex")]
+        public string usersex { get; set; }
+    }
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Tela_Login : ContentPage
     {
@@ -23,8 +39,40 @@ namespace AppPSOne
         }
         private void Entrar(object sender, EventArgs e)
         {
-            Application.Current.Properties["Name"] = Login.Text;
             Navigation.PushAsync(new Calendario());
+        }
+
+        async void Dados(object sender, EventArgs e)
+        {
+            /*var httpClient = new HttpClient();
+            var resultados = await httpClient.GetStringAsync("https://fecap-promoversaude.herokuapp.com/users");
+            Console.WriteLine(resultados);
+
+            var resultadoFinal = JsonConvert.DeserializeObject<DadosUsuario>(resultados);
+
+            Console.WriteLine(resultadoFinal);*/
+            var httpClient = new HttpClient();
+            var novoPost = new DadosUsuario
+            {
+                useremail = "vanessa@teste.com",
+                userpassword = "1234",
+                username = "vruama",
+                usersex = "Feminino"
+            };
+
+            // cria o conteudo da requisição e define o tipo Json
+            var json = JsonConvert.SerializeObject(novoPost);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            // envia a requisição POST
+            var uri = "https://fecap-promoversaude.herokuapp.com/session";
+            var result = await httpClient.PostAsync(uri, content);
+            // Se ocorrer um erro lança uma exceção
+            result.EnsureSuccessStatusCode();
+            // processa a resposta
+            var resultString = await result.Content.ReadAsStringAsync();
+            var post = JsonConvert.DeserializeObject<DadosUsuario>(resultString);
+            // exibe a saida no TextView
+            email.Text = email.ToString();
         }
     }
 }
